d050cdf719d9371f08c477633c71b01e08691f30
nian liu <liunian0523@gmail.com>
2022-02-06 16:36:37 -0500

    modify artist service

--- ArtistWebServer/src/main/java/ArtistsServer.java
+++ ArtistWebServer/src/main/java/ArtistsServer.java
@@ -6,10 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.text.html.Option;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -24,8 +21,6 @@ public class ArtistsServer extends HttpServlet {
         this.artistsCrud = new ArtistsCrud();
     }
 
-
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String nickName = req.getParameter("nickName");
@@ -33,13 +28,13 @@ public class ArtistsServer extends HttpServlet {
         if(nickName == null) {
             List<Artist> la = this.artistsCrud.getAllArtists();
             for(Artist a: la) {
-                out.println(a.toString());
+                out.println(a.getNickName()+". full name: "+a.getFirstName()+" "+a.getLastName());
             }
         }
         else {
             if(this.artistsCrud.existArtist(nickName)) {
                 Artist a1 = this.artistsCrud.readArtist(nickName);
-                out.println(this.artistsCrud.getAllArtists());
+                out.println(a1);
             } else {
                 out.println("No Artist who has nick name: " + nickName);
             }
@@ -49,7 +44,7 @@ public class ArtistsServer extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req,resp);
+        doPut(req,resp);
     }
 
     @Override
@@ -63,14 +58,13 @@ public class ArtistsServer extends HttpServlet {
         BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
         String data = br.readLine();
         String[] args = data.split("&"); // id in args[0], rest in args[1]
-        String[] parts1 = args[0].split("="); // id = parts1[1]
-        nickName = parts1[1];
-        String[] parts2 = args[1].split("="); // parts2[0] is key
-        firstName = parts2[1];
-        String[] parts3 = args[2].split("="); // parts2[0] is key
-        lastName = parts3[1];
-        String[] parts4 = args[3].split("="); // parts2[0] is key
-        shortBio = parts4[1];
+        for(String arg : args) {
+            String[] part = arg.split("=");
+            if(part[0].equals("nickName")) nickName = part[1];
+            if(part[0].equals("firstName")) firstName = part[1];
+            if(part[0].equals("lastName")) lastName = part[1];
+            if(part[0].equals("shortBio")) shortBio = part[1];
+        }
 
         if(nickName!=null&&firstName!=null&&lastName!=null) {
             if (shortBio == null) {
