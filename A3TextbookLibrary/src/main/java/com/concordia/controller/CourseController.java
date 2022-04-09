13bbef78d04b744f55a3770df32883636cdd3f35
heheheliu <70963007+heheheliu@users.noreply.github.com>
2022-04-09 04:34:45 -0400

    update course token

--- A3TextbookLibrary/src/main/java/com/concordia/controller/CourseController.java
+++ A3TextbookLibrary/src/main/java/com/concordia/controller/CourseController.java
@@ -22,119 +22,231 @@ import java.util.List;
 @RequestMapping("/courses")
 public class CourseController {
     private final String googleBooksApiUrl = "https://www.googleapis.com/books/v1/";
+    private String auth_url = "http://localhost:8899/user/auth";
 
     @Autowired
     private CourseService courseService;
 
 
+    private boolean validateToken(String token) {
+        System.out.println(auth_url);
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(String.format(auth_url));
+            httpPost.addHeader("token", token);
+            CloseableHttpResponse httpResponse = client.execute(httpPost);
+            HttpEntity entity = httpResponse.getEntity();
+            String isAuthenticated = EntityUtils.toString(entity);
+            httpResponse.close();
+            if(isAuthenticated.equals("true")){
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
     @GetMapping(path = "getAllCourse", produces = "application/json")
-    public List<Course> getAllCourse(){
-        return courseService.getAllCourse();
+    public List<Course> getAllCourse(@RequestHeader(value = "token") String token){
+        if(validateToken(token)){
+            System.out.println("User is authenticated");
+            return courseService.getAllCourse();
+        }
+        else {
+            System.out.println("User is not authenticated");
+            return null;
+        }
+
     }
 
     @GetMapping(path = "get/{courseNum}",produces = "application/json")
-    public Course getCourseByCourseNum(@PathVariable String courseNum){
-        return courseService.getCourseByCourseNum(courseNum);
+    public Course getCourseByCourseNum(@RequestHeader(value = "token") String token,@PathVariable String courseNum){
+        if(validateToken(token)){
+            System.out.println("User is authenticated");
+            return courseService.getCourseByCourseNum(courseNum);
+        }
+        else {
+            System.out.println("User is not authenticated");
+            return null;
+        }
     }
 
     @PostMapping(path = "createCourse",produces = "application/json")
-    public String createCourse(@RequestParam(name = "courseNum") String courseNum,
+    public String createCourse(@RequestHeader(value = "token") String token,
+                               @RequestParam(name = "courseNum") String courseNum,
                                 @RequestParam(name = "title") String title,
                                 @RequestParam(name = "professor") String professor,
                                 @RequestParam(name = "description") String description,
                                 @RequestParam(name = "keyWord") String keyWord,
                                 @RequestParam(name = "cover") String cover){
-        Boolean result = courseService.createCourse(courseNum, title, professor, description, keyWord, cover);
-        if(result){
-            return "Created new course successfully";
+        if(validateToken(token)){
+            System.out.println("User is authenticated");
+            Boolean result = courseService.createCourse(courseNum, title, professor, description, keyWord, cover);
+            if(result){
+                return "Created new course successfully";
+            }
+            return "Create course fail";
         }
-        return "Create course fail";
+        else {
+            System.out.println("User is not authenticated");
+            return "User is not authenticated";
+        }
+
     }
 
     @PutMapping(path = "updateCourse",produces = "application/json")
-    public String updateCourse(@RequestParam(name = "courseNum") String courseNum,
+    public String updateCourse(@RequestHeader(value = "token") String token,
+                               @RequestParam(name = "courseNum") String courseNum,
                                @RequestParam(name = "title") String title,
                                @RequestParam(name = "professor") String professor,
                                @RequestParam(name = "description") String description,
                                @RequestParam(name = "keyWord") String keyWord,
                                @RequestParam(name = "cover") String cover){
-        Boolean result = courseService.updateCourse(courseNum, title, professor, description, keyWord, cover);
-        if(result){
-            return "Updated course successfully";
+        if(validateToken(token)){
+            System.out.println("User is authenticated");
+            Boolean result = courseService.updateCourse(courseNum, title, professor, description, keyWord, cover);
+            if(result){
+                return "Updated course successfully";
+            }
+            return "Update course fail";
         }
-        return "Update course fail";
-
+        else {
+            System.out.println("User is not authenticated");
+            return "User is not authenticated";
+        }
     }
 
     @DeleteMapping(path = "deleteCourse/{courseNum}",produces = "application/json")
-    public String deleteCourse(@PathVariable String courseNum){
-        if(courseNum.equals("null")||courseNum.length()==0){
-            return "Please enter a course Number";
+    public String deleteCourse(@RequestHeader(value = "token") String token,@PathVariable String courseNum){
+        if(validateToken(token)){
+            System.out.println("User is authenticated");
+            if(courseNum.equals("null")||courseNum.length()==0){
+                return "Please enter a course Number";
+            }
+            Boolean result = courseService.deleteCourse(courseNum);
+            if(result){
+                return "Deleted course successfully";
+            }
+            return "delete course fail";
         }
-        Boolean result = courseService.deleteCourse(courseNum);
-        if(result){
-            return "Deleted course successfully";
+        else {
+            System.out.println("User is not authenticated");
+            return "User is not authenticated";
+
         }
-        return "delete course fail";
+
     }
 
     @GetMapping(path = "getCover/{courseNum}",produces = "application/json")
-    public String getCourseCover(@PathVariable String courseNum){
-        if(courseNum.equals("null")||courseNum.length()==0){
-            return "Please enter a course Number";
+    public String getCourseCover(@RequestHeader(value = "token") String token,@PathVariable String courseNum){
+        if(validateToken(token)){
+            System.out.println("User is authenticated");
+            if(courseNum.equals("null")||courseNum.length()==0){
+                return "Please enter a course Number";
+            }
+            return courseService.getCourseCover(courseNum);
         }
-        return courseService.getCourseCover(courseNum);
+        else{
+            System.out.println("User is not authenticated");
+            return "User is not authenticated";
+        }
+
 
     }
 
     @PutMapping(path = "updateCover/{courseNum}",produces = "application/json")
-    public String updateCourseCover(@PathVariable String courseNum, @RequestParam(name = "cover") String cover){
-        if(courseNum.equals("null")||courseNum.length()==0){
-            return "Please enter a course Number";
+    public String updateCourseCover(@RequestHeader(value = "token") String token,@PathVariable String courseNum, @RequestParam(name = "cover") String cover){
+        if(validateToken(token)){
+            System.out.println("User is authenticated.");
+            if(courseNum.equals("null")||courseNum.length()==0){
+                return "Please enter a course Number";
+            }
+            Boolean result = courseService.updateCourseCover(courseNum,cover);
+            if(result){
+                return "updated course cover successfully";
+            }
+            return "update course cover fail";
         }
-        Boolean result = courseService.updateCourseCover(courseNum,cover);
-        if(result){
-            return "updated course cover successfully";
+        else {
+            System.out.println("User is not authenticated");
+            return "User is not authenticated";
         }
-        return "update course cover fail";
+
+
 
     }
 
     @PostMapping(path = "createComment",produces = "application/json")
-    public String createComment(@RequestParam(name = "username") String username,
+    public String createComment(@RequestHeader(value = "token") String token,
+                                @RequestParam(name = "username") String username,
                                 @RequestParam(name = "courseNum") String courseNum,
                                 @RequestParam(name = "comments") String comments){
-        if(courseNum.equals("null")||courseNum.length()==0||username.equals("null")||username.length()==0){
-            return "Please enter a course Number or an username";
+
+        if(validateToken(token)){
+            System.out.println("User is authenticated.");
+            if(courseNum.equals("null")||courseNum.length()==0||username.equals("null")||username.length()==0){
+                return "Please enter a course Number or an username";
+            }
+            Boolean result = courseService.createComment(username, courseNum, comments);
+            if(result){
+                return "created course comment successfully";
+            }
+            return "create course comment fail";
         }
-        Boolean result = courseService.createComment(username, courseNum, comments);
-        if(result){
-            return "created course comment successfully";
+        else{
+            System.out.println("User is not authenticated");
+            return "User is not authenticated";
         }
-        return "create course comment fail";
+
     }
     @PutMapping(path = "updateComment/{username}/{courseNum}",produces = "application/json")
-    public String updateComment(@PathVariable("username") String username,
+    public String updateComment(@RequestHeader(value = "token") String token,
+                                @PathVariable("username") String username,
                                 @PathVariable("courseNum") String courseNum,
                                 @RequestParam(name = "comments") String comments){
-        if(courseNum.equals("null")||courseNum.length()==0||username.equals("null")||username.length()==0){
-            return "Please enter a course Number or an username";
+        if(validateToken(token)){
+            System.out.println("User is authenticated.");
+            if(courseNum.equals("null")||courseNum.length()==0||username.equals("null")||username.length()==0){
+                return "Please enter a course Number or an username";
+            }
+            Boolean result = courseService.updateComment(username, courseNum, comments);
+            if(result){
+                return "updated course comment successfully";
+            }
+            return "update course comment fail";
         }
-        Boolean result = courseService.updateComment(username, courseNum, comments);
-        if(result){
-            return "updated course comment successfully";
+        else{
+            System.out.println("User is not authenticated");
+            return "User is not authenticated";
         }
-        return "update course comment fail";
+
     }
 
     @GetMapping(path = "getAllComment",produces = "application/json")
-    public List<Comment> getAllComment(){
-        return courseService.getAllComment();
+    public List<Comment> getAllComment(@RequestHeader(value = "token") String token){
+        if(validateToken(token)){
+            System.out.println("User is authenticated.");
+            return courseService.getAllComment();
+        }
+        else{
+            System.out.println("User is not authenticated");
+            return null;
+        }
     }
 
     @GetMapping(path = "getComment/{courseNum}",produces = "application/json")
-    public List<Comment> getCommentByCourseNum(@PathVariable String courseNum){
-        return courseService.getCommentByCourseNum(courseNum);
+    public List<Comment> getCommentByCourseNum(@RequestHeader(value = "token") String token,
+                                               @PathVariable String courseNum){
+        if(validateToken(token)){
+            System.out.println("User is authenticated.");
+            return courseService.getCommentByCourseNum(courseNum);
+        }
+        else{
+            System.out.println("User is not authenticated");
+            return null;
+        }
+
     }
 
 
